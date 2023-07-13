2ad4c9187e72d7fda151e7ee6083e8f0ff1578bd
sbrycbc <sabriyecbc@gmail.com>
2023-07-14 00:16:06 +0200

    updated home style responsive...

--- src/components/Ul/category/Category.jsx
+++ src/components/Ul/category/Category.jsx
@@ -34,7 +34,7 @@ const Category = () => {
     <Row>
     {
         categoryData.map((item,index)=> (
-        <Col lg='3' md='4'>
+        <Col lg='3' md='4' sm='6' xs='6' className='mb-4'>
             <div className="category_item d-flex align-items-center gap-3 mb-4 mt-3">
                 <div className="category_img">
                     <img style={{width:'8rem'}} src={item.imgUrl} alt='category_item' />
