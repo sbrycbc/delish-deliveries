32ec2785a78260822819198ef39899c497b8a2a9
sbrycbc <sabriyecbc@gmail.com>
2023-07-18 15:15:49 +0200

    deleted React.Strictmode from index.js

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ import { AppProvider } from './AppContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+
     <Router>
       <Provider store={store}>
         <AppProvider>
@@ -29,7 +29,7 @@ root.render(
       </Provider>
     </Router>
     
-  </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
